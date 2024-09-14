e0d1cc95ae8d74641bafa753fca8caaae964cc27
Sevde Aydın <74006598+sevdeaydiin@users.noreply.github.com>
2024-09-14 15:55:42 +0300

    Creating MainTabBarViewController

---
+++ NetflixClone/Controllers/DownloadViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DownloadViewController.swift
+//  NetflixClone
+//
+//  Created by Sevde Aydın on 14.09.2024.
+//
+
+import UIKit
+
+class DownloadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
