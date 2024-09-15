c6cb26e4db629af40a268981b40bf0afee2a6328
Sevde Aydın <74006598+sevdeaydiin@users.noreply.github.com>
2024-09-15 23:34:33 +0300

    Sending URL requests and Parsing JSON response

---
+++ NetflixClone/Managers/APICaller.swift
@@ -0,0 +1,41 @@
+//
+//  APICaller.swift
+//  NetflixClone
+//
+//  Created by Sevde Aydın on 15.09.2024.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "697d439ac993538da4e3e60b54e762cd"
+    static let baseURL = "https://api.themoviedb.org"
+}
+
+enum APIError: Error {
+    case failedToGetData
+}
+
+class APICaller {
+    
+    static let shared = APICaller()
+    
+    func getTrendingMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
+        guard let url = URL(string: "\(Constants.baseURL)/3/trending/all/day?api_key=\(Constants.API_KEY)") else { return }
+                
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else { return }
+            
+            do {
+                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                completion(.success(results.results))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+    
+    
+    
+}
