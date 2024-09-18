fe9b6ed2c33c6b2d6abc0bd6e6f659542d4eb2c0
Sevde Aydın <74006598+sevdeaydiin@users.noreply.github.com>
2024-09-18 15:56:49 +0300

    Viewing poster images inside CollectionViewCell

--- NetflixClone/Managers/APICaller.swift
+++ NetflixClone/Managers/APICaller.swift
@@ -43,7 +43,7 @@ class APICaller {
             
             do {
                 let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
-                print(results)
+                completion(.success(results.results))
             } catch {
                 completion(.failure(APIError.failedToGetData))
             }
@@ -58,7 +58,7 @@ class APICaller {
             
             do {
                 let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
-                print(results)
+                completion(.success(results.results))
             } catch {
                 print(APIError.failedToGetData)
             }
@@ -73,7 +73,7 @@ class APICaller {
             
             do {
                 let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
-                print(results)
+                completion(.success(results.results))
             } catch {
                 print(APIError.failedToGetData)
             }
@@ -88,11 +88,12 @@ class APICaller {
             
             do {
                 let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
-                print(results)
+                completion(.success(results.results))
             } catch {
                 print(APIError.failedToGetData)
             }
         }
+        task.resume()
     }
     
     
