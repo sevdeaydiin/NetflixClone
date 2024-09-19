c077369b760edb296107faf52fc00f3ec5f98183
Sevde Aydın <74006598+sevdeaydiin@users.noreply.github.com>
2024-09-19 22:43:30 +0300

    Initial Commit

---
+++ NetflixClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixClone
+//
+//  Created by Sevde Aydın on 19.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
