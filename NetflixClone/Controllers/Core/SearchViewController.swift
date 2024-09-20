a7df68180ce6e28ecd57cd768f238015b86a310f
Sevde Aydın <74006598+sevdeaydiin@users.noreply.github.com>
2024-09-20 19:31:04 +0300

    Querying database for individual movie

--- NetflixClone/Controllers/Core/SearchViewController.swift
+++ NetflixClone/Controllers/Core/SearchViewController.swift
@@ -40,6 +40,8 @@ class SearchViewController: UIViewController {
         navigationController?.navigationBar.tintColor = .foreground
         
         fetchDiscoverMovies()
+        
+        searchController.searchResultsUpdater = self
     }
     
     override func viewDidLayoutSubviews() {
@@ -78,5 +80,28 @@ extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 140
+    }
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        guard let query = searchBar.text,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty,
+              query.trimmingCharacters(in: .whitespaces).count >= 3,
+              let resultsController = searchController.searchResultsController as? SearchResultsViewController else { return }
+        
+        APICaller.shared.search(with: query) { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let titles):
+                    resultsController.titles = titles
+                    resultsController.searchResultsCollectionView.reloadData()
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
     }
 }
